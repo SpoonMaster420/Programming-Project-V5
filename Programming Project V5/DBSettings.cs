06d0bba595d738d3b6cda09f6e18dde203bb879c
SpoonMaster420 <carlansell10@gmail.com>
2023-02-17 15:46:58 +0000

    Changed an icon name

---
+++ Programming Project V5/DBSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Project_V5
+{
+    internal class DBSettings
+    {
+    }
+}
